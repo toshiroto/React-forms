bc5cd7411f9feb83c44515aa9f15c1cd3691c6b0
Carlos Hernandez Topete <72361463+toshiroto@users.noreply.github.com>
2022-03-15 14:16:15 +0900

    Handle Click Implemented

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [click, setClick] = useState(false);
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleClick() {
+    setClick(name);
+  }
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {click} </h1>
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="What's your name?"
+        value={name}
+      />
+      <button onClick={handleClick}>Submit</button>
     </div>
   );
 }
